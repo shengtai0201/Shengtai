52f984c319f3165f80babf70b2d6acacd1ccc622
林聖泰 <shengtai0201@gmail.com>
2020-08-17 10:17:30 +0800

    packages

--- Shengtai.Net.Tests/HDDTests.cs
+++ Shengtai.Net.Tests/HDDTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -48,9 +49,9 @@ namespace Shengtai.Tests
             public double GetWeight()
             {
                 var weight = this.Size * 1.0 / this.Money;
-                //weight *= (this.Rpm * 1.0 / 7200);
-                //weight *= (this.Year * 1.0 / 5);
-                //weight *= (this.Cache * 1.0 / 512);
+                weight *= this.Read * 1.0 / 500;
+                weight *= this.Write * 1.0 / 500;
+                //weight *= this.Year * 1.0 / 3;
 
                 return weight;
             }
@@ -156,18 +157,95 @@ namespace Shengtai.Tests
                 new Sdd { Name = "十銓", Money = 1850, Size = 500, Read = 560, Write = 510, Year = 3 },
                 new Sdd { Name = "十銓", Money = 3390, Size = 1000, Read = 560, Write = 510, Year = 3 },
                 new Sdd { Name = "十銓", Money = 1590, Size = 250, Read = 560, Write = 500, Year = 3 },
+                new Sdd { Name = "十銓", Money = 2390, Size = 500, Read = 560, Write = 510, Year = 3 },
                 new Sdd { Name = "十銓", Money = 4190, Size = 1000, Read = 560, Write = 510, Year = 3 },
                 new Sdd { Name = "十銓", Money = 1890, Size = 250, Read = 560, Write = 500, Year = 3 },
+                new Sdd { Name = "十銓", Money = 2690, Size = 500, Read = 560, Write = 510, Year = 3 },
+                new Sdd { Name = "十銓", Money = 4590, Size = 1000, Read = 560, Write = 510, Year = 3 },
+
+                new Sdd { Name = "UMAX", Money = 749, Size = 240, Read = 520, Write = 450, Year = 3 },
+                new Sdd { Name = "UMAX", Money = 1349, Size = 480, Read = 560, Write = 450, Year = 3 },
+                new Sdd { Name = "UMAX", Money = 2399, Size = 960, Read = 560, Write = 450, Year = 3 },
+
+                new Sdd { Name = "KLEVV", Money = 550, Size = 120, Read = 500, Write = 380, Year = 3 },
+                new Sdd { Name = "KLEVV", Money = 790, Size = 240, Read = 500, Write = 420, Year = 3 },
+                new Sdd { Name = "KLEVV", Money = 1390, Size = 480, Read = 540, Write = 490, Year = 3 },
+                new Sdd { Name = "KLEVV", Money = 699, Size = 240, Read = 520, Write = 500, Year = 3 },
+                new Sdd { Name = "KLEVV", Money = 1278, Size = 480, Read = 520, Write = 500, Year = 3 },    //
+
+                new Sdd { Name = "威剛", Money = 619, Size = 120, Read = 520, Write = 320, Year = 3 },
+                new Sdd { Name = "威剛", Money = 849, Size = 240, Read = 520, Write = 450, Year = 3 },
+                new Sdd { Name = "威剛", Money = 1549, Size = 480, Read = 520, Write = 450, Year = 3 },
+                new Sdd { Name = "威剛", Money = 1090, Size = 256, Read = 560, Write = 520, Year = 3 },
+                new Sdd { Name = "威剛", Money = 1790, Size = 512, Read = 560, Write = 520, Year = 3 },
+                new Sdd { Name = "威剛", Money = 3190, Size = 1000, Read = 560, Write = 520, Year = 3 },
+                new Sdd { Name = "威剛", Money = 6650, Size = 2000, Read = 560, Write = 520, Year = 3 },
+
+                new Sdd { Name = "技嘉", Money = 950, Size = 240, Read = 350, Write = 280, Year = 3 },
+                new Sdd { Name = "技嘉", Money = 1350, Size = 256, Read = 530, Write = 500, Year = 5 },
+                new Sdd { Name = "技嘉", Money = 1999, Size = 512, Read = 530, Write = 500, Year = 5 },
+
+                new Sdd { Name = "Liteon", Money = 599, Size = 120, Read = 560, Write = 460, Year = 3 },
+                new Sdd { Name = "Liteon", Money = 950, Size = 240, Read = 560, Write = 500, Year = 3 },
+                new Sdd { Name = "Liteon", Money = 1499, Size = 480, Read = 560, Write = 500, Year = 3 },
+
+                new Sdd { Name = "Intel", Money = 2180, Size = 256, Read = 550, Write = 500, Year = 5 },
+
+                new Sdd { Name = "Pioneer", Money = 619, Size = 120, Read = 520, Write = 400, Year = 3 },
+                new Sdd { Name = "Pioneer", Money = 820, Size = 240, Read = 520, Write = 450, Year = 3 },
+                new Sdd { Name = "Pioneer", Money = 979, Size = 256, Read = 550, Write = 490, Year = 3 },
+                new Sdd { Name = "Pioneer", Money = 1560, Size = 480, Read = 520, Write = 450, Year = 3 },
+                new Sdd { Name = "Pioneer", Money = 1660, Size = 512, Read = 550, Write = 490, Year = 3 },
+                new Sdd { Name = "Pioneer", Money = 2890, Size = 1000, Read = 550, Write = 500, Year = 3 },
+
+                new Sdd { Name = "WD", Money = 760, Size = 120, Read = 540, Write = 430, Year = 3 },
+                new Sdd { Name = "WD", Money = 890, Size = 240, Read = 540, Write = 430, Year = 3 },
+                new Sdd { Name = "WD", Money = 1550, Size = 480, Read = 540, Write = 430, Year = 3 },
+                new Sdd { Name = "WD", Money = 2880, Size = 1000, Read = 540, Write = 430, Year = 3 },
+                new Sdd { Name = "WD", Money = 5650, Size = 2000, Read = 540, Write = 430, Year = 3 },
+                new Sdd { Name = "WD", Money = 1190, Size = 250, Read = 545, Write = 525, Year = 5 },
+                new Sdd { Name = "WD", Money = 1790, Size = 500, Read = 545, Write = 525, Year = 5 },
+                new Sdd { Name = "WD", Money = 3100, Size = 1000, Read = 560, Write = 530, Year = 5 },
+                new Sdd { Name = "WD", Money = 7150, Size = 2000, Read = 560, Write = 530, Year = 5 },
+                new Sdd { Name = "WD", Money = 14700, Size = 4000, Read = 560, Write = 530, Year = 5 },
+
+                new Sdd { Name = "金士頓", Money = 678, Size = 120, Read = 500, Write = 350, Year = 3 },
+                new Sdd { Name = "金士頓", Money = 899, Size = 240, Read = 500, Write = 350, Year = 3 },
+                new Sdd { Name = "金士頓", Money = 1599, Size = 480, Read = 500, Write = 450, Year = 3 },
+                new Sdd { Name = "金士頓", Money = 2499, Size = 960, Read = 500, Write = 450, Year = 3 },
+                new Sdd { Name = "金士頓", Money = 1250, Size = 256, Read = 550, Write = 500, Year = 5 },
+                new Sdd { Name = "金士頓", Money = 1588, Size = 512, Read = 550, Write = 520, Year = 5 },
+                new Sdd { Name = "金士頓", Money = 3150, Size = 1024, Read = 550, Write = 520, Year = 5 },
+                new Sdd { Name = "金士頓", Money = 1239, Size = 240, Read = 550, Write = 480, Year = 3 },
+                new Sdd { Name = "金士頓", Money = 1949, Size = 480, Read = 550, Write = 480, Year = 3 },
+
+                new Sdd { Name = "美光", Money = 650, Size = 120, Read = 540, Write = 500, Year = 3 },
+                new Sdd { Name = "美光", Money = 850, Size = 240, Read = 540, Write = 500, Year = 3 },
+                new Sdd { Name = "美光", Money = 1669, Size = 480, Read = 540, Write = 500, Year = 3 },
+                new Sdd { Name = "美光", Money = 2799, Size = 1000, Read = 540, Write = 500, Year = 3 },
+                new Sdd { Name = "美光", Money = 5699, Size = 2000, Read = 540, Write = 500, Year = 3 },
+                new Sdd { Name = "美光", Money = 1399, Size = 250, Read = 560, Write = 510, Year = 5 },
+                new Sdd { Name = "美光", Money = 1888, Size = 500, Read = 560, Write = 510, Year = 5 },
+                new Sdd { Name = "美光", Money = 3199, Size = 1000, Read = 560, Write = 510, Year = 5 },
+                new Sdd { Name = "美光", Money = 7499, Size = 2000, Read = 560, Write = 510, Year = 5 },
+
+                new Sdd { Name = "Seagate", Money = 1290, Size = 250, Read = 560, Write = 540, Year = 5 },
+                new Sdd { Name = "Seagate", Money = 1890, Size = 500, Read = 560, Write = 540, Year = 5 },
+                new Sdd { Name = "Seagate", Money = 3390, Size = 1000, Read = 560, Write = 540, Year = 5 },
+                new Sdd { Name = "Seagate", Money = 7390, Size = 2000, Read = 560, Write = 540, Year = 5 },
+
+                new Sdd { Name = "三星", Money = 1488, Size = 250, Read = 550, Write = 520, Year = 5 },
+                new Sdd { Name = "三星", Money = 1888, Size = 500, Read = 550, Write = 520, Year = 5 },
+                new Sdd { Name = "三星", Money = 4750, Size = 1000, Read = 550, Write = 520, Year = 5 },
+                new Sdd { Name = "三星", Money = 2788, Size = 256, Read = 560, Write = 530, Year = 5 },
+                new Sdd { Name = "三星", Money = 4588, Size = 512, Read = 560, Write = 530, Year = 5 },
+                new Sdd { Name = "三星", Money = 9490, Size = 1000, Read = 560, Write = 530, Year = 5 },
+                new Sdd { Name = "三星", Money = 15900, Size = 2000, Read = 560, Write = 530, Year = 5 }
             };
 
-            Sdd s = sdds.First();
-            foreach (var sdd in sdds)
-            {
-                if (sdd.GetWeight() > s.GetWeight())
-                    s = sdd;
-            }
-
-            string test = string.Empty;
+            var sdds1 = sdds.OrderBy(s => s.GetWeight());
+            foreach(var sdd in sdds1)
+                Console.WriteLine($"{sdd.Name}, {sdd.Money}");
         }
     }
 }
