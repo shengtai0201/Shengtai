b6686abce1e30d4bc84794231b126032e62fed0a
林聖泰 <shengtai0201@gmail.com>
2021-03-25 16:02:00 +0800

    menu breadcrumb

--- Shengtai.Core/Web/Telerik/MenuBreadcrumb.cs
+++ Shengtai.Core/Web/Telerik/MenuBreadcrumb.cs
@@ -7,45 +7,90 @@ using System.Threading.Tasks;
 
 namespace Shengtai.Web.Telerik
 {
-    public class MenuBreadcrumb<TRole> where TRole: struct
+    public class MenuBreadcrumb<TKey, TRole>
+        where TKey : struct
+        where TRole : struct
     {
+        public delegate bool ShowEventHandler(MenuBreadcrumb<TKey, TRole> sender, TRole args);
+
         private MenuBreadcrumb()
         {
-            Menus = new List<MenuBreadcrumb<TRole>> { this };
+            Children = new List<MenuBreadcrumb<TKey, TRole>>();
         }
 
-        public MenuBreadcrumb(int id, string name, TRole role) : this()
+        public MenuBreadcrumb(TKey id, string name, TRole role, string icon, ShowEventHandler showEvent) : this()
         {
             this.Id = id;
             this.Name = name;
             this.Role = role;
+            this.Icon = icon;
+            this.ShowEvent += showEvent;
         }
 
-        public MenuBreadcrumb(int id, string name, TRole role, string url) : this(id, name, role)
+        public MenuBreadcrumb(TKey id, string name, TRole role, string icon, ShowEventHandler showEvent, string url)
+            : this(id, name, role, icon, showEvent)
         {
             this.Url = url;
         }
 
-        public int Id { get; set; }
+        public TKey Id { get; set; }
 
         public TRole Role { get; set; }
 
-        public IList<MenuBreadcrumb<TRole>> Menus { get; private set; }
+        public event ShowEventHandler ShowEvent;
+
+        public bool Show(TRole role)
+        {
+            bool result = false;
+
+            // 次目錄是否要顯示
+            foreach (var child in this.Children)
+                result |= child.Show(role);
+
+            if (result && ShowEvent == null)
+                return result;
+            else if (ShowEvent != null)
+                result |= ShowEvent(this, role);
+            else
+                result = false;
+
+            return result;
+        }
 
         [JsonIgnore]
-        public MenuBreadcrumb<TRole> Parent { get; private set; } = null;
+        public MenuBreadcrumb<TKey, TRole> Parent { get; private set; } = null;
 
-        public MenuBreadcrumb<TRole> SetParent(MenuBreadcrumb<TRole> parent)
+        [JsonIgnore]
+        public IList<MenuBreadcrumb<TKey, TRole>> Children { get; private set; }
+
+        /// <summary>
+        /// 作為次層目錄設定上層關聯
+        /// </summary>
+        /// <param name="parent">父節點</param>
+        /// <returns>本節點</returns>
+        public MenuBreadcrumb<TKey, TRole> SetParent(MenuBreadcrumb<TKey, TRole> parent)
         {
             parent.HasChildren = true;
-            this.Parent = parent;
+            parent.Children.Add(this);
 
-            Menus.Clear();
-            Menus = new List<MenuBreadcrumb<TRole>>(this.Parent.Menus)
-            {
-                //this.Parent,
-                this
-            };
+            this.Parent = parent;
+            this.Type = "item";
+
+            return this;
+        }
+
+        /// <summary>
+        /// 作為主層目錄設定上層關聯
+        /// </summary>
+        /// <param name="parent">父節點</param>
+        /// <returns>本節點</returns>
+        public MenuBreadcrumb<TKey, TRole> SetParentByRoot(MenuBreadcrumb<TKey, TRole> parent)
+        {
+            parent.HasChildren = true;
+            parent.Children.Add(this);
+
+            this.Parent = parent;
+            this.Type = "rootitem";
 
             return this;
         }
@@ -54,13 +99,6 @@ namespace Shengtai.Web.Telerik
 
         [JsonProperty("type")]
         public string Type { get; private set; } = "rootitem";
-
-        public MenuBreadcrumb<TRole> SetType(bool isRoot = true)
-        {
-            this.Type = isRoot ? "rootitem" : "item";
-
-            return this;
-        }
 
         [JsonProperty("href")]
         public string Url { get; set; }
@@ -95,9 +133,20 @@ namespace Shengtai.Web.Telerik
         [JsonProperty("showIcon")]
         public bool ShowIcon { get; private set; } = false;
 
+        public static void SetBreadcrumbItems(MenuBreadcrumb<TKey, TRole> menuBreadcrumb, IList<MenuBreadcrumb<TKey, TRole>> value)
+        {
+            if (menuBreadcrumb.Parent != null)
+                SetBreadcrumbItems(menuBreadcrumb.Parent, value);
+
+            value.Add(menuBreadcrumb);
+        }
+
         public string ToBreadcrumbItemsJson()
         {
-            return JsonConvert.SerializeObject(this.Menus, Formatting.None, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+            var value = new List<MenuBreadcrumb<TKey, TRole>>();
+            SetBreadcrumbItems(this, value);
+
+            return JsonConvert.SerializeObject(value, Formatting.None, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
         }
     }
 }
