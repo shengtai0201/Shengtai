658a92091158c46aa2015a8f0623ef0843daf4b4
林聖泰 <shengtai0201@gmail.com>
2021-04-23 22:27:36 +0800

    stock

--- Shengtai.Core.Tests/StockTests.cs
+++ Shengtai.Core.Tests/StockTests.cs
@@ -36,13 +36,87 @@ namespace Shengtai.Core.Tests
         {
             var buy = this.Buy(15.65, 1).Buy;
             buy += this.Buy(15.65, 100).Buy;
-            for (double i = 0; i < 0.1; i += 0.01)
-            {
-                //var price = 15.74 + i;
-                var price = 15.88 + i;
-                var sell = this.Sell(price, 101);
-                Console.WriteLine($"{buy}, {sell}, {price}, {sell - buy}");
-            }
+            var sell = this.Sell(18.04, 101);
+            Console.WriteLine($"{buy}, {sell}, {sell - buy}");
         }
+
+        private DateTime GetDateTime(string value)
+        {
+            var values = value.Split(new[] { "/", "年", "月", "日" }, StringSplitOptions.RemoveEmptyEntries);
+            var year = Convert.ToInt32(values[0]) + 1911;
+            var month = Convert.ToInt32(values[1]);
+            var day = Convert.ToInt32(values[2]);
+
+            return new DateTime(year, month, day);
+        }
+
+        //[Test]
+        //public void BBB()
+        //{
+        //    using var reader = new StreamReader(@"D:\OneDrive\文件\ETF.csv");
+        //    using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        //    var dbContext = new NarcissusDbContext();
+
+        //    csv.Read();
+        //    csv.ReadHeader();
+        //    while (csv.Read())
+        //    {
+        //        var symbol = csv.GetField<string>(0);
+        //        var name = csv.GetField<string>(1);
+
+        //        var exDividendDate = this.GetDateTime(csv.GetField<string>(2));
+        //        var recordDate = this.GetDateTime(csv.GetField<string>(3));
+        //        var paymentDay = this.GetDateTime(csv.GetField<string>(4));
+        //        var amount = csv.GetField<decimal>(5);
+        //        var year = csv.GetField<short>(7);
+
+        //        var stock = dbContext.Stocks.SingleOrDefault(x => x.Symbol == symbol);
+        //        if (stock == null)
+        //        {
+        //            stock = new Stock
+        //            {
+        //                Symbol = symbol,
+        //                Name = name
+        //            };
+        //            dbContext.Stocks.Add(stock);
+        //        }
+
+        //        var etf = dbContext.Etfs.SingleOrDefault(x => x.StockSymbol == symbol && x.ExDividendDate == exDividendDate && x.RecordDate == recordDate && x.PaymentDay == paymentDay);
+        //        if (etf == null)
+        //        {
+        //            stock.Etfs.Add(new Etf
+        //            {
+        //                StockSymbol = symbol,
+        //                ExDividendDate = exDividendDate,
+        //                RecordDate = recordDate,
+        //                PaymentDay = paymentDay,
+        //                Amount = amount,
+        //                Year = year
+        //            });
+        //        }
+
+        //        try
+        //        {
+
+        //            dbContext.SaveChanges();
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            Console.WriteLine(e.Message);
+        //        }
+        //    }
+        //}
+
+        //[Test]
+        //public void CCC()
+        //{
+        //    var dbContext = new NarcissusDbContext();
+        //    var etfs = dbContext.Etfs.Include("StockSymbolNavigation").Where(x => x.StockSymbolNavigation.Price.HasValue).Select(x => new { Symbol = x.StockSymbolNavigation.Symbol, Amount = x.Amount, Price = x.StockSymbolNavigation.Price.Value }).ToList();
+
+        //    var result = etfs.Select(x => new { x.Symbol, Result = x.Amount / x.Price }).OrderByDescending(x => x.Result).Take(56);
+
+        //    Console.WriteLine(result.Count());
+        //}
     }
 }
